bb0cb6df6c2ce520d7685a2692ec60c42bffb056
Dipak Pawar <167563003+dipak-pawar-356@users.noreply.github.com>
2025-01-16 00:51:05 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,12 +20,12 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='https://uber-video-6arn.vercel.app/' element={<Start />} />
+        <Route path='/' element={<Start />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/riding' element={<Riding />} />
         <Route path='/captain-riding' element={<CaptainRiding />} />
 
-        <Route path='https://uber-video-6arn.vercel.app/signup' element={<UserSignup />} />
+        <Route path='/signup' element={<UserSignup />} />
         <Route path='/captain-login' element={<Captainlogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
         <Route path='/home'
